06646a09b152d6133500eff15c471b1beec2f7ad
Nathan Wick <nathanwick2001@gmail.com>
2023-10-12 14:10:18 -0400

    Store User Data

---
+++ lib/models/user.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String name;
+  final String email;
+  final String photo;
+
+  User({
+    required this.name,
+    required this.email,
+    required this.photo,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'photo': photo,
+    };
+  }
+
+  User.fromDocumentSnapshot(DocumentSnapshot snapshot)
+    : name = snapshot['name'],
+      email = snapshot['email'],
+      photo = snapshot['photo'];
+}
